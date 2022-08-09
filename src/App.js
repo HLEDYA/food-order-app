58bc3ea397a8115383567de4ec439776cb803955
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-09 12:07:20 +0300

    show and hide cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartVisible(true);
+    console.log("showCartHandler called");
+  };
+
+  const hideCartHandler = () => {
+    setIsCartVisible(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {isCartVisible && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
